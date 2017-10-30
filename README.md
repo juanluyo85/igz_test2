f06e0d8d493a679e8f4b9b460a9bcc90808a224a
Juan Luis Gutiérrez Blanco <juanluis.gutierrez.contractor@bbva.com>
2017-10-30 11:11:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Prueba 2
+========
+
+En esta prueba se debe completar la función encode en el archivo utils/compression.py
+
+## Ejecución del programa
+Ejecutar el programa usando python2:
+### Unix
+```bash
+python2 main.py
+```
+### Windows
+```
+python2.exe main.py
+```
---
+++ utils/compression.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+def encode(in_str):
+    """
+    Esta función debe devolver el string codificado de la siguiente manera:
+    Por cada grupo de caracteres alfabéticos (de la A a la Z sin incluir la Ñ y sin distinguir mayúsculas
+    de minúsculas) iguales consecutivos, se incluira dicho caracter en mayúsculas seguido del número de repeticiones.
+    Por ejemplo para la entrada "aaAabaccCBb", la salida debe ser "A4B1A1C3B2"
+    :param in_str: string de entrada. Estará formado por letras de la A a la Z sin incluir la Ñ
+    :return: string codificado como se describe más arriba
+    """
+    pass
