77a18cfb66022db4cd56ef2761e9bfbb5960f80d
Juan Andrés <you@example.com>
2017-11-13 18:05:06 +0100

    Implementación del módulo utils/compression    * Función encode

--- utils/compression.py
+++ utils/compression.py
@@ -10,4 +10,16 @@ def encode(in_str):
     :param in_str: string de entrada. Estará formado por letras de la A a la Z sin incluir la Ñ
     :return: string codificado como se describe más arriba
     """
-    pass
+    in_str = in_str.upper()
+    current_char = in_str[0]
+    current_count = 0
+    out_str = ''
+    for ch in in_str:
+        if(ch == current_char):
+            current_count += 1
+        else:
+            out_str += current_char + str(current_count)
+            current_count = 1
+            current_char = ch  
+    out_str += current_char + str(current_count)
+    return out_str
